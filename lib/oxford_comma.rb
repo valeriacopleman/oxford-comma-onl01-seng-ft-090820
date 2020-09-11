e837c212128081fd87b74c65778b8373d58460ba
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-11 01:05:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  
+if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    return "#{array[0..-2].join(", ")}, and #{array.last}"
+  end
 end
